64b1bbe4285fc6a451db50f16d01a8733815aa84
Djebreds <refiahmad.djebreds@gmail.com>
2024-12-04 16:50:25 +0700

    feat: update transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,11 @@
 # Represents a transaction in the application.
 class Transaction < ApplicationRecord
   belongs_to :user
+
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
+
+  enum :transaction_type, {
+    top_up: 'top-up',
+    purchase: 'purchase',
+  }
 end
