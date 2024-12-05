46c96567d374d3e2aca051ca91be89b388fe9196
Djebreds <refiahmad.djebreds@gmail.com>
2024-12-05 16:46:47 +0700

    feat: update top up functionality

--- app/controllers/top_ups_controller.rb
+++ app/controllers/top_ups_controller.rb
@@ -8,16 +8,27 @@ class TopUpsController < ApplicationController
     amount = top_up_params[:amount].to_d
     payment_method = top_up_params[:payment_method]
 
-    redirect_to root_path, alert: 'Amount must be a valid positive number.' if invalid_amount?(amount)
+    validate_amount!(amount)
+    validate_payment_method!(payment_method)
 
-    redirect_to root_path, alert: 'Invalid payment method' unless invalid_payment_method?(payment_method)
+    add_credit = TopUpServices::AddCredit.call(current_user, amount, payment_method)
 
-    TopUpServices::AddCredit.call(current_user, amount, payment_method)
-
-    redirect_to root_path, notice: 'Your credit has been successfully updated!'
+    if add_credit
+      redirect_to root_path, notice: 'Your credit has been successfully updated!'
+    else
+      redirect_to top_up_path, alert: 'Failed to topup your account.'
+    end
   end
 
   private
+
+  def validate_amount!(amount)
+    redirect_to root_path, alert: 'Amount must be a valid positive number.' if invalid_amount?(amount)
+  end
+
+  def validate_payment_method!(payment_method)
+    redirect_to root_path, alert: 'Invalid payment method' unless invalid_payment_method?(payment_method)
+  end
 
   def top_up_params
     params.permit(:amount, :payment_method)
--- app/services/top_up_services/add_credit.rb
+++ app/services/top_up_services/add_credit.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module TopUpServices
-  # Perform top up credit for user
+  # Perform top-up credit for user
   class AddCredit < ApplicationService
     def initialize(user, amount, payment_method)
       @user = user
@@ -10,15 +10,23 @@ module TopUpServices
     end
 
     def call
-      raise ArgumentError, 'Amount must be greater than zero' unless valid_amount?
-
-      user.update_credit(@amount)
-      user.create_transaction(@user, @amount, :top_up, @payment_method)
+      ActiveRecord::Base.transaction do
+        validate_amount!
+        user.update_credit(amount)
+        user.create_transaction(user, amount, :top_up, payment_method)
+      end
+    rescue StandardError => e
+      Rails.logger.error e.message
+      false
     end
 
     private
 
-    attr_reader :user, :amount
+    attr_reader :user, :amount, :payment_method
+
+    def validate_amount!
+      raise ArgumentError, 'Amount must be greater than zero' unless valid_amount?
+    end
 
     def valid_amount?
       amount.present? && amount.positive?
